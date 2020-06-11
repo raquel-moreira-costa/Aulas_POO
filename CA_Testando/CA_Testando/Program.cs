47c13ab0fae5de4d71ba651037d39db7009bc4d4
RachMor <raquel.costt@gmail.com>
2020-06-11 14:54:12 -0300

    Primeiro commit

---
+++ CA_Testando/CA_Testando/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CA_Testando
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Testando sincronização do VS2019 com o GitHub");
+            Console.ReadKey();
+        }
+    }
+}
